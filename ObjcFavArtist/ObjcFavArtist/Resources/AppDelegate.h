4bf00569de9017dd4276421acb98dc975995938f
Michael R.F <mrflowers22@uwalumni.com>
2019-11-09 06:17:04 -0800

    organized project's folders

---
+++ ObjcFavArtist/ObjcFavArtist/Resources/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ObjcFavArtist
+//
+//  Created by Michael Flowers on 11/9/19.
+//  Copyright © 2019 Michael Flowers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
